36e608b3596c11626603e38066ff266bc4deb34e
Bryl Magdadaro <asapsherbs@gmail.com>
2026-06-16 19:58:26 +0800

    feat(app): wire HTTP client, auth interceptor, and app initializer

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
-import { provideZonelessChangeDetection } from '@angular/core';
+import { inject, provideAppInitializer, provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules, withComponentInputBinding } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { AppInitializer } from './app/core/initializers/app.initializer';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthHeadersInterceptor } from './app/core/interceptors/auth-headers.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideAppInitializer(() => inject(AppInitializer).initializeApp()),
     provideZonelessChangeDetection(),
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules), withComponentInputBinding()),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHeadersInterceptor,
+      multi: true,
+    },
   ],
-});
+}).catch((err: any) => console.error(err));
