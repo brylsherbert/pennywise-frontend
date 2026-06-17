63d4c49a8ef46f7a1889aa5d97f6778a97caeb9c
Bryl Magdadaro <asapsherbs@gmail.com>
2026-06-17 19:34:09 +0800

    refactor(pages): remove default Ionic tab pages

--- src/app/layout/tabs/tabs.routes.ts
+++ src/app/layout/tabs/tabs.routes.ts
@@ -7,30 +7,15 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadComponent: () =>
-          import('../../pages/tab1/tab1.page').then((m) => m.Tab1Page),
-      },
-      {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../../pages/tab2/tab2.page').then((m) => m.Tab2Page),
-      },
-      {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../../pages/tab3/tab3.page').then((m) => m.Tab3Page),
-      },
-      {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/budgets',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/budgets',
     pathMatch: 'full',
   },
 ];
